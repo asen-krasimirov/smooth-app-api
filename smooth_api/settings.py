adc6e40d11a12c56a149924537a6f1871dd3197a
asen-krasimirov <68907559+asen-krasimirov@users.noreply.github.com>
2021-11-16 12:10:08 +0200

    fixing collectstatic

--- smooth_api/settings.py
+++ smooth_api/settings.py
@@ -97,6 +97,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_DIR, 'staticfiles'),
+)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
